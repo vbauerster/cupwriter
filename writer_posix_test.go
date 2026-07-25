550d98c5df16fea6e2b63c8853f6cb9454c8c31d
Vladimir Bauer <vbauerster@users.noreply.github.com>
2026-07-26 00:46:44 +0500

    test (*Writer).Flush

---
+++ writer_posix_test.go
@@ -0,0 +1,65 @@
+//go:build !windows
+
+package cupwriter
+
+import (
+	"bytes"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestWriterFlush(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		lines int
+	}{
+		{"single", strings.Repeat("foo\n", 1), 1},
+		{"double", strings.Repeat("foo\n", 2), 2},
+		{"multi", strings.Repeat("foo\n", 3), 3},
+	}
+
+	out := new(bytes.Buffer)
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			defer out.Reset()
+			cw := New(out, true)
+			// need at least 2 flushes for esc chars to appear
+			for range 2 {
+				cw.WriteString(test.input)
+				cw.Flush(test.lines)
+			}
+			p, err := parse(out.String())
+			if err != nil {
+				t.Fatal(err)
+			}
+			if p.input != test.input {
+				t.Errorf("want: %q, got %q", test.input, p.input)
+			}
+			if p.lines != test.lines {
+				t.Errorf("want: %d, got %d", test.lines, p.lines)
+			}
+		})
+	}
+}
+
+type parsed struct {
+	input string
+	lines int
+}
+
+func parse(escaped string) (p *parsed, err error) {
+	p = new(parsed)
+	for i, sep := range []string{escOpen, cuuAndEd} {
+		if before, after, found := strings.Cut(escaped, sep); found {
+			switch i {
+			case 0:
+				p.input, escaped = before, after
+			case 1:
+				p.lines, err = strconv.Atoi(before)
+			}
+		}
+	}
+	return
+}
