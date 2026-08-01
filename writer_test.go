98636542490195d4371330af06fcb85d01f99e10
Vladimir Bauer <vbauerster@users.noreply.github.com>
2026-08-01 11:19:44 +0500

    minor: make input from test.lines

--- writer_test.go
+++ writer_test.go
@@ -10,22 +10,22 @@ import (
 func TestWriterFlush(t *testing.T) {
 	testCases := []struct {
 		name  string
-		input string
 		lines int
 	}{
-		{"single", strings.Repeat("foo\n", 1), 1},
-		{"double", strings.Repeat("foo\n", 2), 2},
-		{"multi", strings.Repeat("foo\n", 3), 3},
+		{"single", 1},
+		{"double", 2},
+		{"multi", 3},
 	}
 
 	out := new(bytes.Buffer)
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			defer out.Reset()
+			input := strings.Repeat("foo\n", test.lines)
 			cw := New(out, true)
 			// need at least 2 flushes for esc chars to appear
 			for range 2 {
-				cw.WriteString(test.input)
+				cw.WriteString(input)
 				err := cw.Flush(test.lines)
 				if err != nil {
 					t.Fatal(err)
@@ -35,8 +35,8 @@ func TestWriterFlush(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			if p.input != test.input {
-				t.Errorf("want: %q, got %q", test.input, p.input)
+			if p.input != input {
+				t.Errorf("want: %q, got %q", input, p.input)
 			}
 			if p.lines != test.lines {
 				t.Errorf("want: %d, got %d", test.lines, p.lines)
